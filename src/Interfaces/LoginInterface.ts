a717027fed1043d3759db3143585f8c0e1def7b6
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-10-15 14:33:32 +0300

    Implemented form validation in the Login component

---
+++ src/Interfaces/LoginInterface.ts
@@ -0,0 +1,6 @@
+export interface LoginInterface {
+    username: string,
+    password: string,
+    usernameError: string,
+    passwordError: string,
+}
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import {FaRegUser} from 'react-icons/fa';
 import {BsKey} from 'react-icons/bs';
+import { LoginInterface } from '../Interfaces/LoginInterface';
 
 function Login() {
+
+    const [login, setLogin] = useState<LoginInterface>({
+        username: '',
+        password: '',
+        usernameError: '',
+        passwordError: '',
+    })
+
+    const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setLogin({...login, [e.target.name] : e.target.value});
+    }
+
+    const validate = () => {
+        let usernameError = "";
+        let passwordError = "";
+
+        if (!login.username) {
+            usernameError = 'Username cannot be empty';
+        }
+        if (!login.password) {
+            passwordError = 'Password cannot be empty'
+        }
+
+        if (passwordError) {
+            setLogin({...login, passwordError : passwordError});
+            return false;
+        }
+
+        if (usernameError) {
+            setLogin({...login, usernameError : usernameError});
+            return false;
+        }
+        return true;
+    }
+
+    const handleSubmit = (e : React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const isValid : boolean = validate();
+        if (isValid) {
+            console.log(login);
+            setLogin({...login, username : '', password : '',
+                usernameError : '', passwordError : ''});
+        }
+    }
+
     return (
         <div className="sm:w-1/3 w-3/4 h-1/2 ml-auto mr-auto mt-24">
             <div className="w-full flex justify-center">
@@ -12,7 +58,7 @@ function Login() {
                 </h1>
             </div>
             <div className="w-full flex flex-col justify-center items-center pt-4">
-                <form className="sm:w-1/2 w-full">
+                <form onSubmit={handleSubmit} className="sm:w-1/2 w-full">
                     <div className="mt-6 border-b border-gray-400">
                         <label className="text-sm font-semibold">
                             Username
@@ -21,9 +67,15 @@ function Login() {
                             <FaRegUser className="text-gray-400" />
                             <input
                                 className="w-full p-3"
+                                type="text"
                                 name="username"
                                 placeholder="Type your username"
+                                value={login.username}
+                                onChange={handleChange}
                             />
+                        </div>
+                        <div className="text-red-600">
+                            {login.usernameError}
                         </div>
                     </div>
                     <div className="mt-4 border-b border-gray-400">
@@ -34,13 +86,19 @@ function Login() {
                             <BsKey className="text-gray-400" />
                             <input
                                 className="w-full p-3"
+                                type="password"
                                 name="password"
                                 placeholder="Type your password"
+                                value={login.password}
+                                onChange={handleChange}
                             />
+                        </div>
+                        <div className="text-red-600">
+                            {login.passwordError}
                         </div>
                     </div>
                     <div className="w-full flex flex-col mt-6 text-white">
-                        <button className="bg-indigo-800 p-2">
+                        <button type="submit" className="bg-indigo-800 p-2">
                             Login
                         </button>
                         <Link to="/register" className="bg-indigo-800 p-2 text-center mt-6">
